a87bfbede11c0927cc6bb3f40a19f2628e4b7636
zhong yang <it.yangzhong@outlook.com>
2018-03-14 23:45:07 +0800

    测试

---
+++ Hello/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 100000000; i++)
+            {
+
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.ElapsedTicks);
+        }
+    }
+}
